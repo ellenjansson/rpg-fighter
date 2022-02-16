0c0fa3160e526592597afc31b3e60c5344513ac6
ellenjansson <ellen04jansson@gmail.com>
2022-02-16 15:08:44 +0100

    message

--- src/resources.py
+++ src/resources.py
@@ -8,34 +8,36 @@ class Character:
         self.attack = attack
         self.armor = armor
     
-    def __str__(self) -> str:
+    def __str__(self):
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
-        
-    def damage(self):
-        return self.attack
     
     def take_damage(self, damage):
         relative_damage = damage - self.armor
         self.health -= relative_damage
         if self.health < 0: self.health = 0
     
+    def get_attack(self):
+        return self.attack
+    
     def get_health(self):
         return self.health
+    
+    def get_name(self):
+        return self.name
         
     
 class Goblin:
     
     
-    def __init__(self, health, attack, armor):
+    def __init__(self, health, attack, armor, id):
         self.health = health
         self.attack = attack
         self.armor = armor
+        self.id = id
     
-    def __str__(self) -> str:
-        return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
-    
-    def damage(self):
-        return self.attack
+    def __str__(self):
+        return f"Goblin #{self.id}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+
     
     def take_damage(self, damage):
         relative_damage = damage - self.armor
@@ -44,5 +46,11 @@ class Goblin:
     
     def get_health(self):
         return self.health
+    
+    def get_attack(self):
+        return self.attack
+    
+    def get_name(self):
+        return f"Goblin #{self.id}"
         
     
