fa89d4b88f77b9254d7939f63553dd99e596d7f4
ellenjansson <ellen04jansson@gmail.com>
2022-02-16 13:58:39 +0100

    progress 2022-02-15

--- src/resources.py
+++ src/resources.py
@@ -9,4 +9,40 @@ class Character:
         self.armor = armor
     
     def __str__(self) -> str:
-        return_string = f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+        
+    def damage(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+    
+    def get_health(self):
+        return self.health
+        
+    
+class Goblin:
+    
+    
+    def __init__(self, health, attack, armor):
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+    
+    def __str__(self) -> str:
+        return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+    
+    def damage(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+    
+    def get_health(self):
+        return self.health
+        
+    
